b4750dc1d7189b3a495ae5472b8728478d7c44ad
Abhishek Sahani <abhisheksahani@Abhisheks-MacBook-Air.local>
2024-01-30 14:27:57 +0530

    build: project setup for nextjs 14

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies, import/extensions */
+import './src/libs/Env.mjs';
+import withBundleAnalyzer from '@next/bundle-analyzer';
+import withNextIntl from 'next-intl/plugin';
+
+const withNextIntlConfig = withNextIntl('./src/libs/i18n.ts');
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+/** @type {import('next').NextConfig} */
+

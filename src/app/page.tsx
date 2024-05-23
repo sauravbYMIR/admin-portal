1f43f1f5c9fa734df38dc3e5c1b7746c399a9296
Saurav Biswas <biswassaurav71@gmail.com>
2024-05-23 18:47:16 +0530

    chore: image alt name change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ function LandingPage() {
         <Image
           className={style.bannerImg}
           src={landingPageBanner}
-          alt="landing page banner image"
+          alt="landing page banner imag"
         />
       </div>
     </div>
